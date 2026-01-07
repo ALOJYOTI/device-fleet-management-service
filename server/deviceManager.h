4a39047086aad805cdef892fb986912da13d1dca
Alojyoti Mistry <alojyotimistry@gmail.com>
2026-01-07 02:45:41 +0000

    add initial device manager class structure

---
+++ server/deviceManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <shared_mutex>
+#include <unordered_map>
+#include <string>
+
+class DeviceManager
+{
+public:
+    bool RegisterDevice(const std::string& id, devicefleet::DeviceState state);
+    bool SetDeviceStatus(const std::string& id, devicefleet::DeviceState state);
+    bool GetDeviceInfo(const std::string& id, devicefleet::Device& device);
+private:
+    std::unordered_map<std::string, devicefleet::Device> devices_;
+    mutable std::shared_mutex mutex;
+};
---
+++ server/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Device Fleet Server starting..." << std::endl;
+    // TODO grpc server
+    return 0;
+}
